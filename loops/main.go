c5052589eb9cdb84f4e9604347ed3adfc04af46e
Darvin Sinaga <nagacoder@outlook.com>
2019-03-02 23:04:19 +0700

    add newer file

--- loops/main.go
+++ loops/main.go
@@ -2,5 +2,26 @@ package main
 import "fmt"
 
 func  main()  {
-	fmt.Println("Hello Word")
+	//long method
+	i := 1
+	for i <=10 {
+		fmt.Println(i)
+		i++
+	}
+	// short method 
+	for i:= 1; i <=10; i++{
+		fmt.Println("Number", i)
+	}
+	//fizzbuzz
+	for i:= 1; i <=100; i++ {
+		if i % 15 == 0 {
+			fmt.Println("fizzbuzz")
+		}else if i % 5 ==0 {
+			fmt.Println("buzz")
+		}else if i % 3 == 0{
+			fmt.Println("fizzz")
+		}else {
+			fmt.Println(i)
+		}
+	}
 }
